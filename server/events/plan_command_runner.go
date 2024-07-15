d096f895a76fdd71fcbd6bc0930a0e6001d2ba3e
Andrei Vydrin <tufitko@gmail.com>
2024-07-15 13:55:48 +0700

    feat: force clone before plan

--- server/events/plan_command_runner.go
+++ server/events/plan_command_runner.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pkg/errors"
 	"strconv"
 
+	"github.com/runatlantis/atlantis/server/core/config/valid"
 	"github.com/runatlantis/atlantis/server/core/locking"
 	"github.com/runatlantis/atlantis/server/events/command"
 	"github.com/runatlantis/atlantis/server/events/models"
@@ -145,7 +146,7 @@ func (p *PlanCommandRunner) runAutoplan(ctx *command.Context) {
 			}
 
 			// Lock the project
-			lockResponse, err := p.projectLocker.TryLock(pctx.Log, pctx.Pull, pctx.User, pctx.Workspace, models.NewProject(pctx.Pull.BaseRepo.FullName, pctx.RepoRelDir, pctx.ProjectName), pctx.RepoLocking)
+			lockResponse, err := p.projectLocker.TryLock(pctx.Log, pctx.Pull, pctx.User, pctx.Workspace, models.NewProject(pctx.Pull.BaseRepo.FullName, pctx.RepoRelDir, pctx.ProjectName), pctx.RepoLocksMode == valid.RepoLocksOnPlanMode)
 			if err != nil {
 				pctx.Log.Err("locking project: %s", err)
 				lockResult.Error = errors.Wrap(err, "acquiring lock")
@@ -305,7 +306,7 @@ func (p *PlanCommandRunner) run(ctx *command.Context, cmd *CommentCommand) {
 			}
 
 			// Lock the project
-			lockResponse, err := p.projectLocker.TryLock(pctx.Log, pctx.Pull, pctx.User, pctx.Workspace, models.NewProject(pctx.Pull.BaseRepo.FullName, pctx.RepoRelDir, pctx.ProjectName), pctx.RepoLocking)
+			lockResponse, err := p.projectLocker.TryLock(pctx.Log, pctx.Pull, pctx.User, pctx.Workspace, models.NewProject(pctx.Pull.BaseRepo.FullName, pctx.RepoRelDir, pctx.ProjectName), pctx.RepoLocksMode == valid.RepoLocksOnPlanMode)
 			if err != nil {
 				pctx.Log.Err("locking project: %s", err)
 				lockResult.Error = errors.Wrap(err, "acquiring lock")
